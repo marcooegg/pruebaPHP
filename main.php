1b657a05102088ab89367f2ae27fbdc27cb8452b
Marcosoft <marco.oegg@hotmail.com>
2017-09-12 21:44:14 -0300

    agregar main

---
+++ main.php
@@ -0,0 +1,19 @@
+<?
+//En caso de * o / deberan ingresarse los polinomios en distintos <input de tipo texto>
+//Ubicar signos + -
+//Agregar "_" delante y detras
+//Ubicar similitudes entre los distintos inputs
+//Decidir cual es el polinomio que representa U (Posiblemente el de x con mayor exponente)
+//Analizar termino e integrar = dU
+//Modificar dU para que sea su valor sea el mismo que el de U
+//Reemplazar los terminos por U y dU
+//Integrar segun tabla
+//Reemplazar U y dU por los terminos originales
+
+$formulas = array();
+for ($i=0; $i < length($inputs); $i++) 
+{ 
+	 $formulas[$i] = $inputs[$i];
+	 
+}
+?>
